37f9aa63e31c05aea806f0c3de7d2f17f30a2677
Becca Petrin <beccapetrin@gmail.com>
2020-07-21 09:35:44 -0700

    Do-over

--- sdk/go/approzium/util/util.go
+++ sdk/go/approzium/util/util.go
@@ -1,13 +1,16 @@
 package util
 
-import "io/ioutil"
+import (
+	"fmt"
+	"io/ioutil"
+)
 
 // ReadFile exists because sometimes ioutil.ReadFile returns both
 // a "file name too long" error, and the actual body of the file.
 // We only want to worry about the error if the file can't be read.
 func ReadFile(filename string) ([]byte, error) {
 	result, err := ioutil.ReadFile(filename)
-	if len(result) == 0 && err != nil {
+	if fmt.Sprintf("%s", result) == "" && err != nil {
 		return nil, err
 	}
 	return result, nil
