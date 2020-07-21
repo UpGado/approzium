3e088d91e84926ac3bd837402eb68f884a9e4b6f
Becca Petrin <beccapetrin@gmail.com>
2020-07-21 09:43:08 -0700

    What am I getting

--- sdk/go/approzium/util/util.go
+++ sdk/go/approzium/util/util.go
@@ -9,7 +9,11 @@ import (
 // a "file name too long" error, and the actual body of the file.
 // We only want to worry about the error if the file can't be read.
 func ReadFile(filename string) ([]byte, error) {
+	fmt.Println("I got here")
+	fmt.Println("Reading " + filename)
 	result, err := ioutil.ReadFile(filename)
+	fmt.Printf("result is %s", result)
+	fmt.Printf("err is %s", err)
 	if fmt.Sprintf("%s", result) == "" && err != nil {
 		return nil, err
 	}
